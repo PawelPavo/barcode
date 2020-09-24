7891fdf13e1ac5707adc9b7777f14c13d140b7f8
PawelPavo <Pawelebllc@gmail.com>
2020-09-24 13:04:51 -0400

    latest updated

--- src/client/views/CreateOffer.tsx
+++ src/client/views/CreateOffer.tsx
@@ -2,18 +2,38 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 import { useLocation, useHistory, Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
-const Barcode = require('react-barcode');
+
+const offers = [
+    {
+        "name": "BOGO",
+        "SKU": 123
+    },
+    {
+        "name": "10%OFF",
+        "SKU": 456
+    },
+    {
+        "name": "FREE-DAY-PASS",
+        "SKU": 789
+    },
+]
 
 const CreateOffer: React.FC<CreateOfferProps> = (props) => {
     const history = useHistory();
     const location = useLocation<{ msg: string }>();
     const [name, setName] = useState<string>('');
-    const [description, setDescription] = useState<string>('');
-    const [type, setType] = useState<string>('');
-    
+    const [keycode, setKeycode] = useState<string>('');
+    const [newOffer, setNewOffer] = useState<{}>({name, keycode})
+
     const generate = async (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
-        history.push('/');
+        try {
+            setNewOffer({name, keycode})
+            console.log(newOffer)
+        } catch (error) {
+            
+        }
+        history.push('/createoffer');
     }
 
 
@@ -22,24 +42,29 @@ const CreateOffer: React.FC<CreateOfferProps> = (props) => {
             <Navbar />
 
             <section className="row justify-content-center my-5">
-
                 <form className="form-group border-primary rounded-lg-success">
-
+                    <div className="form-group">
+                        <label>Offers</label>
+                        <select
+                            className="form-control"
+                            value={name}
+                            onChange={e => setName(e.target.value)}>
+                            {offers.map(offer => (
+                                <option>{offer.name} SKU:{offer.SKU}</option>
+                            ))}
+                        </select>
+                    </div>
                     <div className="shadow p-4 mx-8 bg-white">
                         <h3 className="text-center"> Create Offer </h3>
-
-                        <label>Marketing Offer Name</label>
-                        <input value={name} onChange={e => setName(e.target.value)} className="form-control" />
-
-                        <label>Marketing Offer Description</label>
-                        <input value={description} onChange={e => setDescription(e.target.value)} className="form-control" />
+                        <label>Key Code</label>
+                        <input
+                            value={keycode}
+                            onChange={e => setKeycode(e.target.value)}
+                            className="form-control" />
 
                         <label>Promotion Type</label>
-                        <input value={type} onChange={e => setType(e.target.value)} className="form-control" />
-
-                        <button onClick={generate} className="btn">Generate Bar Codes</button>
-
-                        <button onClick={() => history.push('/')} className="btn">Go Back</button>
+                        <input value={name} onChange={e => setName(e.target.value)} className="form-control" />
+                        <button onClick={generate} className="btn btn-outline-secondary mt-3 btn-block">Generate Offer</button>
 
                     </div>
                 </form>
